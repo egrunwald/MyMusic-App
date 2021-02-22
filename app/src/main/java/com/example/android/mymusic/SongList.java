105e2d3655823e391f21eb24abbca133df128259
egrunwald <egrunwald112@gmail.com>
2021-02-21 16:15:46 -0800

    added Attribution for all songs

--- app/src/main/java/com/example/android/mymusic/SongList.java
+++ app/src/main/java/com/example/android/mymusic/SongList.java
@@ -1,4 +1,49 @@
 package com.example.android.mymusic;
+/*Attributions for All Songs Used In This App
+ *
+ * Artist: Jens East
+ * Song: Nightrise
+ * Artist: Jens East (featuring Diandra Faye)
+ * Song: Galaxies
+ * Artist: Jens East (featuring Camilla North)
+ * Song: Invisible
+ * CC BY
+ * http://www.jenseast.com/
+ * www.soundcloud.com/jenseast
+ * Licence: Creative Commons Attribution V4.0
+ * https://creativecommons.org/licenses/by/4.0
+ *
+ * Artist: Computer Music Allstars
+ * Song: Clueless
+ * Song: May the Chords be With You
+ * CC BY
+ * http://www.cmallstars.com
+ * Licence: Creative Commons Attribution V4.0
+ * https://creativecommons.org/licenses/by/4.0
+ *
+ * Artist: Dancefloor is Lava
+ * Song: Drown in Noise
+ * Song: Control
+ * Song: Why oh You Are Love
+ * Public Domain
+ * http://loyaltyfreakmusic.com
+ * License: CC0 1.0 Universal/Public Domain Dedication
+ * https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Artist: Jaunitos
+ * Song: Del Carnaval
+ * CC BY
+ * http://www.juanitos.net
+ * License: Attribution 2.0 France
+ * https://creativecommons.org/licenses/by/2.0/fr/
+ *
+ * Artist: Tintamare
+ * Song: Propane
+ * CC BY-SA
+ * https://tintamare.bandcamp.com
+ * License: Attribution-ShareAlike 4.0 International
+ * https://creativecommons.org/licenses/by-sa/4.0/
+ */
 
 import java.util.ArrayList;
 
